56566937a7dfd9c86ee706f35d76e74131064a03
rpalbq <rebecalbuq0@gmail.com>
2022-04-06 22:23:19 -0300

    adc ex03

---
+++ exercicios/ex03.py
@@ -0,0 +1,6 @@
+#Desenvolva um programa que efetue a leitura de dois valores numéricos inteiros. Processe a operação de adição dos dois valores e apresente uma nova variável com o resultado.
+valor1 = float(input('Digite um valor: '))
+valor2 = float(input('Digite outro valor: '))
+
+soma = valor1 + valor2
+print('A soma entre os dois valores é de:', soma)
