d499aef22d78a080646700d7e3b3b473e10bbd61
rpalbq <rebecalbuq0@gmail.com>
2022-04-14 01:54:18 -0300

    adc ex14 ex15

---
+++ exercicios/ex14.py
@@ -0,0 +1,8 @@
+#Retorne quantos caracteres tem na string "I dream of you almost every night"
+
+musica = "I dream of you almost every night"
+print(len(musica))
+print(musica[4])
+print(musica[6:15])
+print(musica.upper())
+print(musica.lower())
