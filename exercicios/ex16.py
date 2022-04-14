0c2c6849d0674b86d0560b5c80daecea01599049
rpalbq <rebecalbuq0@gmail.com>
2022-04-14 14:47:12 -0300

    adc ex16

---
+++ exercicios/ex16.py
@@ -0,0 +1,10 @@
+#Crie um programa que receba 5 valores do usuário, e imprima a soma dos dois primeiros mais a soma dos dois últimos, depois, multiplique o resultado pelo valor do meio.
+
+valor1 = float(input('Digite o primeiro valor:'))
+valor2 = float(input('Digite o segundo valor:'))
+valor3 = float(input('Digite o terceiro valor:'))
+valor4 = float(input('Digite o quarto valor:'))
+valor5 = float(input('Digite o quinto valor:'))
+
+total =  (valor1 + valor2 + valor4 + valor5) * valor3
+print(total)
